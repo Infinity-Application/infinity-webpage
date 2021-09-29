4e569545d79c85b2a8bfbf50551b1a0cf08e57cf
Nadeemmazahreh <nadeemmazahreh97@hotmail.com>
2021-09-29 16:07:48 +0300

    CSS is almost finished

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,18 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
-import heroImage from './CryptoImage1.jpg'
+import logo from './Infinity-logo.png'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import './Homepage.css'
+
+
 
 export default function Homepage() {
     return (
-        <div>
-            <div>
-                <h1>#1 Crypto App in the middle east</h1>
-                <h1>*Launch App*</h1>
-                
-            </div>
-            <div>
-                <img src={heroImage} alt="Logo" style={{width:500}} />
-            </div>
-        </div>
+            <Row xs={1} md={2} className="g-4" style={{backgroundColor:'#202020'}}>
+              <Col>
+                <Card style={{backgroundColor:'#202020'}}>
+                  <Card.Body className='left-body'>
+                    <Card.Title>#1 Crypto App in the Middle East</Card.Title>
+                    <Button variant="primary">Launch app</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{backgroundColor:'#202020'}}>
+                  <Card.Img  src={logo} style={{width:400}}/>
+                </Card>
+              </Col>
+          </Row>
     )
 }

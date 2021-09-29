bef5805acb135c39f927898a860ea5b953455c88
Nadeemmazahreh <nadeemmazahreh97@hotmail.com>
2021-09-29 14:22:39 +0300

    webpage created

---
+++ src/component/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import {
+    BrowserRouter ,
+    Switch,
+    Route,
+    NavLink
+  } from "react-router-dom";
+import './Navbar.css'
+
+export default function Navbar() {
+    const [click, setClick] = React.useState(false);
+  
+    const handleClick = () => setClick(!click);
+    const Close = () => setClick(false);
+    
+    return (
+      <div>
+       <div className={click ? "main-container" : ""} onClick={()=>Close()} />
+        <nav className="navbar" onClick={(e)=>e.stopPropagation()}>
+          <div className="nav-container">
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/"
+                  activeClassName="active"
+                  className="nav-links"
+                  onClick={click ? handleClick : null}
+                >
+                  Home
+                </NavLink>
+              </li>
+              
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/contact"
+                  activeClassName="active"
+                  className="nav-links"
+                 onClick={click ? handleClick : null}
+                >
+                  Contact Us
+                </NavLink>
+              </li>
+            </ul>
+            <div className="nav-icon" onClick={handleClick}>
+              <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+            </div>
+          </div>
+        </nav>
+      </ div>
+    );
+}
